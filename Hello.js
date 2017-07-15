d716a9bf28cba1f2a889c9f27318bd463d1102f5
emilj858 <teamgoout@gmail.com>
2017-07-15 13:38:17 -0700

    First commit, boiler plate code

---
+++ Hello.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+
+export default class Hello extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text style= {styles.hello}>Hello from Hello.js</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    hello: {
+        backgroundColor: 'aqua',
+        fontSize: 24
+    }
+});
