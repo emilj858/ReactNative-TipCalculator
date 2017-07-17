77b6f1a5aa00d9d83d358cdc9be957edc9f278f3
emilj858 <teamgoout@gmail.com>
2017-07-16 17:06:05 -0700

    added input value

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 import Hello from './Hello';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      inputValue:""
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <View style ={styles.view1}></View>
-        <View style ={styles.view2}></View>
+        <Text>
+          {this.state.inputValue}
+        </Text>
+        <TextInput placeholder='Enter bill ammount'
+          style={styles.input}
+          value={this.state.inputValue}
+          onChangeText={(text)=> this.setState({inputValue: text})}
+         
+        />
       </View>
     );
   }
@@ -17,20 +31,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: 20,
   },
-  view1: {
-     backgroundColor: '#ff00ff',
-     flex:1,
-     height: 100,
-  },
-  view2: {
-      backgroundColor: '#00ffff',
-      flex: 1,
-      height: 100,
-  } 
-
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 7,
+  }
 });
